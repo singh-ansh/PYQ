e6457864e1c4a8616faa382494449fcbe0f78e16
Ansh <ayush5798ji@gmail.com>
2025-12-20 18:46:16 +0530

    Add files via upload

---
+++ styl.js
@@ -0,0 +1,32 @@
+let Math = document.querySelector(".content");
+let science = document.querySelector(".pdf-browser");
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        // document.body.style.backgroundColor = "yellow";
+        let inp = prompt("Enter the subject Math or Science :");
+        if (inp === "math" || inp === "Math" || inp === "Mathematics" || inp === "mathematics" || inp === "MATHEMATICS") {
+            science.replaceWith(Math);
+        } else if (inp === "Science" || inp === "science" || inp === "Sci" || inp === "sci" || inp === "SCIENCE") {
+            Math.replaceWith(science);
+        } else {
+            alert("Not Available ! ");
+            // Math.remove();
+            // science.remove();
+        }
+    } else {
+        // document.body.style.backgroundColor = "pink";
+
+    }
+}
+
+// Create a MediaQueryList object
+let x = window.matchMedia("(max-width: 700px)")
+
+// Call listener function at run time
+myFunction(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+    myFunction(x);
+});
---
+++ styl1.js
@@ -0,0 +1,68 @@
+// let Twenty_five = document.querySelector(".tf");
+// let Twenty_five_sets = document.querySelector(".tfs");
+// let Twenty_four = document.querySelector(".Twenty-four");
+// let Twenty_four_sets = document.querySelector(".Twenty-four-sets");
+// let Twenty_three = document.querySelector(".Twenty-three");
+// let Twenty_three_sets = document.querySelector(".Twenty-three-sets");
+// Twenty_five_sets.remove();
+// Twenty_four_sets.remove();
+// Twenty_three_sets.remove();
+
+
+// Twenty_five.addEventListener("click", () => {
+//     Twenty_five.append(Twenty_five_sets);
+//     Twenty_five.addEventListener("click", () => {
+//         Twenty_five_sets.remove();
+//         Twenty_five.addEventListener("click", () => {
+//             Twenty_five.append(Twenty_five_sets);
+//             Twenty_five.addEventListener("click", () => {
+//                 Twenty_five_sets.remove();
+//                 Twenty_five.addEventListener("click", () => {
+//                     Twenty_five.append(Twenty_five_sets);
+//                     Twenty_five.addEventListener("click", () => {
+//                         Twenty_five_sets.remove();
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+
+// Twenty_four.addEventListener("click", () => {
+//     Twenty_four.append(Twenty_four_sets);
+//     Twenty_four.addEventListener("click", () => {
+//         Twenty_four_sets.remove();
+//         Twenty_four.addEventListener("click", () => {
+//             Twenty_four.append(Twenty_four_sets);
+//             Twenty_four.addEventListener("click", () => {
+//                 Twenty_four_sets.remove();
+//                 Twenty_four.addEventListener("click", () => {
+//                     Twenty_four.append(Twenty_four_sets);
+//                     Twenty_four.addEventListener("click", () => {
+//                         Twenty_four_sets.remove();
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
+
+
+// Twenty_three.addEventListener("click", () => {
+//     Twenty_three.append(Twenty_three_sets);
+//     Twenty_three.addEventListener("click", () => {
+//         Twenty_three_sets.remove();
+//         Twenty_three.addEventListener("click", () => {
+//             Twenty_three.append(Twenty_three_sets);
+//             Twenty_three.addEventListener("click", () => {
+//                 Twenty_three_sets.remove();
+//                 Twenty_three.addEventListener("click", () => {
+//                     Twenty_three.append(Twenty_three_sets);
+//                     Twenty_three.addEventListener("click", () => {
+//                         Twenty_three_sets.remove();
+//                     });
+//                 });
+//             });
+//         });
+//     });
+// });
